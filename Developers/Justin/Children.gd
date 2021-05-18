e806301f751dd1e0c35ce47dfd4526481cc6aa19
Justin <justp02@hotmail.com>
2021-05-18 00:12:06 -0500

    Added Children Paths (WIP)

--- Developers/Justin/Children.gd
+++ Developers/Justin/Children.gd
@@ -11,10 +11,42 @@ enum DESTINATIONS { # 8 of them estimated
 	BATHROOM
 }
 
-export(DESTINATIONS) var StartingDoor
-export(DESTINATIONS) var EndingDoor
-
+export(NodePath) var patrol_path
+#export(DESTINATIONS) var StartingDoor
+#export(DESTINATIONS) var EndingDoor
 export(int) var speed = 2000
 
 var target := Vector2.ZERO
 var movement := Vector2.ZERO
+
+var patrol_points
+var patrol_index = 0
+
+func _ready() -> void:
+	var ChosenPath = randi()%2 #get a random integer between 0 and 1
+	match ChosenPath:
+		1:patrol_path="../Paths/GroceryTOClothing"
+			
+		2:patrol_path="../Paths/GroceryTOHome1"
+		
+	if self.patrol_path != null:
+		self.patrol_points = self.get_node(patrol_path).curve.get_baked_points()
+
+#	StartingDoor = randi()%9 #get a random integer between 0 and 8
+#	EndingDoor = randi()%9 #get a random integer between 0 and 8
+
+		
+
+func _physics_process(delta: float) -> void:
+
+	if self.patrol_path == null:
+		return
+	
+	self.target = self.patrol_points[self.patrol_index]
+	
+	if self.position.distance_to(target) < 1:
+		self.patrol_index = wrapi(self.patrol_index + 1, 0, self.patrol_points.size())
+		self.target = self.patrol_points[self.patrol_index]
+	
+	self.movement = (self.target - self.position).normalized() * self.speed * delta
+	self.movement = self.move_and_slide(self.movement)
