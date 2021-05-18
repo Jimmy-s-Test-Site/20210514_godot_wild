574224ef23de3c68c8e881b5db36762352698443
Justin <justp02@hotmail.com>
2021-05-17 22:40:50 -0500

    Children Re-push

--- Developers/Justin/Children.gd
+++ Developers/Justin/Children.gd
@@ -1,11 +1,6 @@
 extends KinematicBody2D
 
-enum STATE {
-	ROAM,
-#	CHASE
-}
-
-enum TARGETDOOR { # 8 of them estimated
+enum DESTINATIONS { # 8 of them estimated
 	CLOTHING,
 	GROCERY,
 	HOME1,
@@ -16,32 +11,10 @@ enum TARGETDOOR { # 8 of them estimated
 	BATHROOM
 }
 
-export(int) var speed = 2000
-#export(NodePath) var player_path
+export(DESTINATIONS) var StartingDoor
+export(DESTINATIONS) var EndingDoor
 
-#onready var player : Node2D = self.get_node_or_null(self.player_path)
-onready var state : int = STATE.ROAM
+export(int) var speed = 2000
 
 var target := Vector2.ZERO
 var movement := Vector2.ZERO
-
-#func _physics_process(delta: float) -> void:
-#	match self.state:
-#		STATE.ROAM:
-#			pass
-#		STATE.CHASE:
-#			self.move_and_slide((self.target * self.speed * delta).normalized())
-
-#func _on_Area2D_body_entered(body: Node) -> void:
-#	print("entered :P")
-#	if self.player != null:
-#		self.target = self.global_position.direction_to(self.player.global_position)
-#		self.state = STATE.CHASE
-#	else:
-#		self.target = Vector2.ZERO
-#		self.state = STATE.ROAM
-#
-#
-#func _on_Area2D_body_exited(body: Node) -> void:
-#	self.target = Vector2.ZERO
-#	self.state = STATE.ROAM
